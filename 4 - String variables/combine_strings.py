131fc94477f65b4a5ecd8d1ae8c9dc35bb1d3baa
Rajasekhar Matam <sekhar.matam@gmail.com>
2020-04-16 09:14:07 +0530

    learning python

---
+++ 4 - String variables/combine_strings.py
@@ -0,0 +1,10 @@
+# You can use the + operator to concatenate strings
+print("Please Enter First Name:")
+first_name =  input()
+print("Please Enter Last Name:")
+last_name =  input()
+#print(first_name + last_name)
+
+# If you want a space between the strings you must include the space
+# within the string
+print('Hello ' + first_name + ' ' + last_name)
