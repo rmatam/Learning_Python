e906346bf13ad8ea4229a77402f8bf87d510b621
Rajasekhar Matam <sekhar.matam@gmail.com>
2020-05-31 20:50:44 +0530

    19-Numpy

---
+++ 19-Numpy/Creating_Arrays.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+a = np.array([1,2,3])
+#print(a)
+
+b = np.array([(1.5,2,3),(4,5,6)],dtype = float)
+#print  (b)
+
+c = np.array([[(1.5,2,3),(4,5,6)],[(3,2,1), (4,5,6)]], dtype = float)
+print  (c)
---
+++ 19-Numpy/initialplaceholders.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+a = np.zeros((3,4))
+#print(a)
+
+b = np.zeros((9,4))
+#print(b)
+
+c = np.ones((2,3,4))
+#print(c)
+
+d = np.ones((2,3,4), dtype = np.int16)
+#print(d)
+
+e = np.arange(10,25,5)
+print(e)
+
+f = np.arange(10,25,1.5)
+print(f)
+
+g = np.arange(10,25,1)
+print(g)
